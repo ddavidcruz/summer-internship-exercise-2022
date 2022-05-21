9a511d9116b840896435a07f640850d8bdfaaed3
ddavidcruz98 <davidcruz@live.com.pt>
2022-05-21 11:31:33 +0100

    first commit

--- src/main/java/com/premiumminds/internship/screenlocking/ScreenLockinPattern.java
+++ src/main/java/com/premiumminds/internship/screenlocking/ScreenLockinPattern.java
@@ -1,12 +1,14 @@
 package com.premiumminds.internship.screenlocking;
 
 import java.util.concurrent.Future;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 /**
  * Created by aamado on 05-05-2022.
  */
 class ScreenLockinPattern implements IScreenLockinPattern {
 
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
  /**
   * Method to count patterns from firstPoint with the length
   * @param firstPoint initial matrix position
@@ -14,6 +16,11 @@ class ScreenLockinPattern implements IScreenLockinPattern {
   * @return number of patterns
   */
   public Future<Integer> countPatternsFrom(int firstPoint,int length) {
-    throw new RuntimeException("Not Implemented Yet");
+      System.out.println("Teste");
+      return executor.submit(() -> {
+          Thread.sleep(1000);
+          return 5;
+      });
+    //throw new RuntimeException("Not Implemented Yet");
   };
 }
